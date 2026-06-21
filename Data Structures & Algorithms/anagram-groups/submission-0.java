4affd069b7978d27f3dd4375c041377cb3f67c57
sayendutta <44966615+sayendutta@users.noreply.github.com>
2026-06-21 13:36:00 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+
+            int[] freq = new int[26];
+
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+            
+
+            //we are using freq array itself as the key
+            for (int count : freq) {
+                key.append('#').append(count);
+            }
+            //if a string matches with the key (for each string key is computed)
+            //add to the map
+            map.computeIfAbsent(key.toString(),
+                    k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
