75e523ad248d90aff2d3ae5bea32e765d9d835cc
sayendutta <44966615+sayendutta@users.noreply.github.com>
2026-07-05 00:19:06 +0530

    Add: relative-sort-array - submission-1

---
+++ Data Structures & Algorithms/relative-sort-array/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        // convert arr2 into hashset to speed up searching
+        Set<Integer> arr2_set = new HashSet<>();
+        for (int num : arr2) arr2_set.add(num);
+        Map<Integer, Integer> arr1_freq = new HashMap<>();
+        List<Integer> endList = new ArrayList<>();
+        //need to count the frequency of elements in arr1
+        for (int num : arr1) {
+            if (!arr2_set.contains(num))
+                endList.add(num);
+            else
+                arr1_freq.put(num, arr1_freq.getOrDefault(num, 0) + 1);
+        }
+        //Sort the ending portion
+        //as we need to sort the elements not in arr2 in ascending order
+        Collections.sort(endList);
+        List<Integer> res = new ArrayList<>();
+        //form the first portion of resultant array
+        for (int num : arr2) {
+            for (int i = 0; i < arr1_freq.get(num); i++) res.add(num);
+        }
+        res.addAll(endList);
+        return res.stream()
+               .mapToInt(Integer::intValue)
+               .toArray();
+    }
+}
