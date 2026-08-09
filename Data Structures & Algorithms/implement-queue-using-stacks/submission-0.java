2924c6414903c821e8499616914ae0c3f36345f1
sayendutta <44966615+sayendutta@users.noreply.github.com>
2026-08-09 14:48:25 +0530

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.java
@@ -0,0 +1,48 @@
+class MyQueue {
+    Stack<Integer> s1, s2;
+    //elements will stay in s1
+    //s2 will be a temporary stack to help implement FIFO
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        while (s1.size() > 1) {
+            s2.push(s1.pop());
+        }
+        int res = s1.pop();
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+        return res;
+    }
+
+    public int peek() {
+        while (s1.size() > 1) {
+            s2.push(s1.pop());
+        }
+        int res = s1.peek();
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+        return res;
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
